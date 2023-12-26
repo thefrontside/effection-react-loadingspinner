0b23e2b6ac948f9883747a4f860950c40cd754d7
Taras Mankovski <taras@frontside.com>
2023-12-26 11:31:13 -0500

    Improved responsive abit

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -14,33 +14,33 @@ export function Player({ title, description, op }: PlayerProps) {
 
   return (
     <>
-      <div className="grid lg:grid-cols-2 lg:gap-2">
+      <div className="grid sm:grid-cols-2 sm:gap-2">
         <div>
           <h3 className="text-lg">{title}</h3>
           <p className="text-slate-600">{description}</p>
         </div>
-        <div className="grid grid-cols-6 gap-4 mt-2 lg:mt-0">
+        <div className="flex flex-col sm:flex-row mt-2 lg:mt-0">
           {playing ? (
             <>
               <button
-                className="rounded bg-red-800 text-white p-4"
+                className="rounded bg-red-800 text-white p-4 mb-2 sm:mb-0"
                 onClick={() => setPlaying(false)}
               >
                 Stop
               </button>
-              <div className="border border-1 rounded border-solid border-blue-800 p-2 col-span-5">
+              <div className="border border-1 rounded border-solid border-blue-800 p-2 grow sm:ml-4">
                 <Runner op={op} />
               </div>
             </>
           ) : (
             <>
               <button
-                className="rounded bg-blue-800 text-white p-4"
+                  className="rounded bg-blue-800 text-white p-4 mb-2 sm:mb-0"
                 onClick={() => setPlaying(true)}
               >
                 Play
               </button>
-              <div className="border border-1 rounded border-solid border-slate-100 p-2 col-span-5"></div>
+                <div className="border border-1 rounded border-solid border-slate-100 p-2 grow sm:ml-4"></div>
             </>
           )}
         </div>
