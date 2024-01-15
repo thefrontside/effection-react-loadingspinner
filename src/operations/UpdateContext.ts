08bf94ff52aa6c523fb517ad9769a97e30d96c35
Taras Mankovski <taras@frontside.com>
2024-01-14 19:51:17 -0500

    Removed unnecessarily typed update type

--- src/operations/UpdateContext.ts
+++ src/operations/UpdateContext.ts
@@ -1,16 +1,14 @@
 import { Operation, createContext, lift } from "effection";
 import { LoaderState } from "../hooks/useLoader";
 
-type Update<T> = (state: LoaderState<T>) => Operation<void>;
+export const UpdateContext = createContext<typeof update>("update");
 
-export const UpdateContext = createContext<Update<unknown>>("update");
-
-export const update: Update<unknown> = function* update(value) {
+export const update = function* update<T>(value: LoaderState<T>): Operation<void> {
   const setState = yield* UpdateContext;
   yield* setState(value);
 }
 
-export function* setUpdate(setState: (value: LoaderState<unknown>) => void): Operation<Update<unknown>> {
+export function* setUpdate(setState: (value: LoaderState<unknown>) => void): Operation<typeof update> {
   const lifted = lift(setState);
   yield* UpdateContext.set(lifted);
   return lifted;
